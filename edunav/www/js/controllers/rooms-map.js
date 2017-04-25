0e43f586955ee3bb8b7103c9e73fd0fdfca1a470
Maartje Eyskens <meyskens@me.com>
2017-04-25 15:19:30 +0200

    Fix misformat on rooms-map

--- edunav/www/js/controllers/rooms-map.js
+++ edunav/www/js/controllers/rooms-map.js
@@ -1,8 +1,22 @@
-angular.module("edunav.controllers").controller("RoomsMapCtrl", roomsMapController)
+angular
+    .module("edunav.controllers")
+    .controller("RoomsMapCtrl", roomsMapController)
 
-roomsMapController.$inject = ["$scope", "BackendService", "$ionicLoading", "$stateParams", "StorageService"]
+roomsMapController.$inject = [
+    "$scope",
+    "BackendService",
+    "$ionicLoading",
+    "$stateParams",
+    "StorageService",
+]
 
-function roomsMapController($scope, BackendService, $ionicLoading, $stateParams, StorageService) {
+function roomsMapController(
+    $scope,
+    BackendService,
+    $ionicLoading,
+    $stateParams,
+    StorageService
+) {
     $scope.isFavorite = false
     var canvas = document.getElementById("room-map-canvas")
     canvas.width = document.documentElement.clientWidth
@@ -10,57 +24,63 @@ function roomsMapController($scope, BackendService, $ionicLoading, $stateParams,
     var ctx = canvas.getContext("2d")
     var image = new Image()
 
-	// image starting points
-	                                                                                                                        var xStart
-	                                                                                                                        var yStart
-	                                                                                                                        var imageWidth
-	                                                                                                                        var imageHeight
+    // image starting points
+    var xStart
+    var yStart
+    var imageWidth
+    var imageHeight
 
-	                                                                                                                        var showOnCanvas = function() {
-		                                                                                                                        var imageAspectRatio = image.width / image.height
-		                                                                                                                        var canvasAspectRatio = canvas.width / canvas.height
+    var showOnCanvas = function() {
+        var imageAspectRatio = image.width / image.height
+        var canvasAspectRatio = canvas.width / canvas.height
 
-		                                                                                                                        imageWidth = canvas.height
-		                                                                                                                        imageHeight = canvas.width
-		                                                                                                                        xStart = 0
-		                                                                                                                        yStart = 0
+        imageWidth = canvas.height
+        imageHeight = canvas.width
+        xStart = 0
+        yStart = 0
 
-		                                                                                                                        if (imageAspectRatio < canvasAspectRatio) {
-			// fit on height
-			                                                                                                                        imageHeight = canvas.height
-			                                                                                                                        imageWidth = image.width * (imageHeight / image.height)
-			                                                                                                                        xStart = (canvas.width - imageWidth) / 2
-			                                                                                                                        yStart = 0
-		}
+        if (imageAspectRatio < canvasAspectRatio) {
+            // fit on height
+            imageHeight = canvas.height
+            imageWidth = image.width * (imageHeight / image.height)
+            xStart = (canvas.width - imageWidth) / 2
+            yStart = 0
+        }
 
-		                                                                                                                        if (imageAspectRatio > canvasAspectRatio) {
-			// fit on height
-			                                                                                                                        imageWidth = canvas.width
-			                                                                                                                        imageHeight = image.height * (imageWidth / image.width)
-			                                                                                                                        xStart = 0
-			                                                                                                                        yStart = (canvas.height - imageHeight) / 2
-		}
-		                                                                                                                        ctx.drawImage(image, xStart, yStart, imageWidth, imageHeight)
-	}
+        if (imageAspectRatio > canvasAspectRatio) {
+            // fit on height
+            imageWidth = canvas.width
+            imageHeight = image.height * (imageWidth / image.width)
+            xStart = 0
+            yStart = (canvas.height - imageHeight) / 2
+        }
+        ctx.drawImage(image, xStart, yStart, imageWidth, imageHeight)
+    }
 
-	                                                                                                                        var drawCircle = function(x,y) {
-		                                                                                                                        ctx.beginPath()
-		                                                                                                                        ctx.arc(x * imageWidth + xStart, y * imageHeight + yStart, 10, 0, 2*Math.PI)
-		                                                                                                                        ctx.stroke()
-	}
+    var drawCircle = function(x, y) {
+        ctx.beginPath()
+        ctx.arc(
+            x * imageWidth + xStart,
+            y * imageHeight + yStart,
+            10,
+            0,
+            2 * Math.PI
+        )
+        ctx.stroke()
+    }
 
     $ionicLoading.show({
-        template: "<ion-spinner></ion-spinner>"
+        template: "<ion-spinner></ion-spinner>",
     })
-    $scope.$on( "$ionicView.loaded", function() {
-        BackendService.getMap($stateParams.map).then((response) => {
+    $scope.$on("$ionicView.loaded", function() {
+        BackendService.getMap($stateParams.map).then(response => {
             $scope.map = response.data
             image.src = $scope.map.imageLocation
             image.onload = function() {
                 showOnCanvas()
-                BackendService.getRoom($stateParams.room).then((response) => {
-					                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        drawCircle(response.data.x, response.data.y)
-            	                                                                                                                        })
+                BackendService.getRoom($stateParams.room).then(response => {
+                    drawCircle(response.data.x, response.data.y)
+                })
                 $ionicLoading.hide()
             }
         })
